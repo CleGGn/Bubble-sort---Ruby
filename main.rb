44bde9bc20cbb93f221899a3c2f4712ecc0ca7b2
Clément <gaugainc@yahoo.com>
2024-08-20 18:08:16 +0200

    Resolved

--- main.rb
+++ main.rb
@@ -1,7 +1,30 @@
 class Test
+  # Build a method #bubble_sort that takes an array and returns a sorted
+  # array. It must use the bubble sort methodology (using #sort would be
+  # pretty pointless, wouldn’t it?).
 
   def bubble_sort(arr)
-
+    sorted = false
+    count = 0
+    array_length = arr.length
+    store = 0
+    while sorted == false
+      count = 0
+      arr.select.with_index do |value, index|
+        if index  != array_length -1
+          if (arr[index] > arr[index+1])
+            store = arr[index+1]
+            arr[index+1] = arr[index]
+            arr[index] = store
+            count = count +1
+          end
+        end
+      end
+      if count == 0
+        sorted = true
+      end
+    end
+    arr
   end
 
 end 
